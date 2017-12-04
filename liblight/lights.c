1ebaf31bbe2368ed7038860bf0fac2ed4738926d
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-12-04 13:10:45 +0200

    mido: lights: Add back buttons lights

--- liblight/lights.c
+++ liblight/lights.c
@@ -46,13 +46,13 @@ char const*const RED_LED_FILE
         = "/sys/class/leds/red/brightness";
 
 char const*const RED_BLINK_FILE
-	= "/sys/class/leds/red/blink";
+	    = "/sys/class/leds/red/blink";
 
 char const*const GREEN_BLINK_FILE
-	= "/sys/class/leds/green/blink";
+	    = "/sys/class/leds/green/blink";
 
 char const*const BLUE_BLINK_FILE
-	= "/sys/class/leds/blue/blink";
+	    = "/sys/class/leds/blue/blink";
 
 char const*const GREEN_LED_FILE
         = "/sys/class/leds/green/brightness";
@@ -62,6 +62,9 @@ char const*const BLUE_LED_FILE
 
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
+
+char const*const BUTTON_FILE
+        = "/sys/class/leds/button-backlight/brightness";
 
 char const*const RED_BREATH_FILE
         = "/sys/class/leds/red/led_time";
@@ -436,6 +439,20 @@ set_light_attention(struct light_device_t* dev,
     return 0;
 }
 
+static int
+set_light_buttons(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int err = 0;
+    if(!dev) {
+        return -1;
+    }
+    pthread_mutex_lock(&g_lock);
+    err = write_int(BUTTON_FILE, state->color & 0xFF);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
 /** Close the lights device */
 static int
 close_lights(struct light_device_t *dev)
@@ -466,6 +483,8 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
     else
